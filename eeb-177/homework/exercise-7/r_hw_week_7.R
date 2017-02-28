f1aefd44aabc1dcc5b0cd7ebf8890d646659584f
zjgold <zack.j.gold@gmail.com>
2017-02-27 16:45:53 -0800

    homework 7

---
+++ eeb-177/homework/exercise-7/r_hw_week_7.R
@@ -0,0 +1,45 @@
+1+100
+
+1:5
+2^(1:5)
+
+ls()
+
+min_height =0
+min_height=0
+max.height=0
+#_age=0
+.mass=0
+MaxLength=0
+min-length=0
+#2widths=0
+celsius2kelvin=0
+#Challenge 1
+#min_height =0
+#min_height=0
+#max.height=0
+#.mass=0 but it's hidden
+#MaxLength=0
+#celsius2kelvin=0
+
+#Challenge 2
+mass <- 47.5
+age <- 122
+mass <- mass * 2.3
+age <- age - 20
+# mass = 109.25
+# age = 102
+
+#Challenge 3
+age>mass
+#False, correct answer is mass>age
+
+# Challenge 4
+rm(age,mass)
+ls()
+
+#Challenge 5
+install.packages('ggplot2')
+install.packages('plyr')
+install.packages('gapminder')
+
